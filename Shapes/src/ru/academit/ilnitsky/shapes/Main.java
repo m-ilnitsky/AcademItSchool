f7ddf584e4316b32c2f5fe5d1137a10fbb0d241d
Mike <ilnitsky@ngs.ru>
2016-10-01 01:05:35 +0700

    Первы коммит!

---
+++ Shapes/src/ru/academit/ilnitsky/shapes/Main.java
@@ -0,0 +1,7 @@
+package ru.academit.ilnitsky.shapes;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World of Packages, Objects and Version Controls!!!");
+    }
+}
