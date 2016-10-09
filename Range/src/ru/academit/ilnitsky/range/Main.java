a0777884602cfbc50c3b1f8653bd6dfef6c213b4
Mike <ilnitsky@ngs.ru>
2016-10-10 01:12:37 +0700

    Работа над ошибками. Удален проверка на null для разницы в Main из .range

--- Range/src/ru/academit/ilnitsky/range/Main.java
+++ Range/src/ru/academit/ilnitsky/range/Main.java
@@ -96,41 +96,33 @@ public class Main {
         System.out.println();
 
         Range[] range6d7 = range6.calcDifference(range7);
-        if(range6d7!=null) {
-            System.out.print("range6.calcDifference(range7) = ");
-            for (int i = 0; i < range6d7.length; i++) {
-                System.out.print(range6d7[i] + " ");
-            }
-            System.out.println();
+        System.out.print("range6.calcDifference(range7) = ");
+        for (int i = 0; i < range6d7.length; i++) {
+            System.out.print(range6d7[i] + " ");
         }
+        System.out.println();
 
         Range[] range3d7 = range3.calcDifference(range7);
-        if(range3d7!=null) {
-            System.out.print("range3.calcDifference(range7) = ");
-            for (int i = 0; i < range3d7.length; i++) {
-                System.out.print(range3d7[i] + " ");
-            }
-            System.out.println();
+        System.out.print("range3.calcDifference(range7) = ");
+        for (int i = 0; i < range3d7.length; i++) {
+            System.out.print(range3d7[i] + " ");
         }
+        System.out.println();
 
         System.out.println();
 
         Range[] range6u7 = range6.calcUnion(range7);
-        if(range6u7!=null) {
-            System.out.print("range6.calcUnion(range7) = ");
-            for (int i = 0; i < range6u7.length; i++) {
-                System.out.print(range6u7[i] + " ");
-            }
-            System.out.println();
+        System.out.print("range6.calcUnion(range7) = ");
+        for (int i = 0; i < range6u7.length; i++) {
+            System.out.print(range6u7[i] + " ");
         }
+        System.out.println();
 
         Range[] range3u7 = range3.calcUnion(range7);
-        if(range3u7!=null) {
-            System.out.print("range3.calcUnion(range7) = ");
-            for (int i = 0; i < range3u7.length; i++) {
-                System.out.print(range3u7[i] + " ");
-            }
-            System.out.println();
+        System.out.print("range3.calcUnion(range7) = ");
+        for (int i = 0; i < range3u7.length; i++) {
+            System.out.print(range3u7[i] + " ");
         }
+        System.out.println();
     }
 }
