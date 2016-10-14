0808ab6fffd06d2d7fbd1e3eb300b56cc81a9f24
m-ilnitsky <ilnitsky@ngs.ru>
2016-10-14 19:44:21 +0700

    Matrix: почти всё реализовано.

--- Matrix/src/ru/academit/ilnitsky/matrix_utils/Main.java
+++ Matrix/src/ru/academit/ilnitsky/matrix_utils/Main.java
@@ -91,9 +91,84 @@ public class Main {
         for (int i = 0; i < mat3.length; i++) {
             System.out.println("[" + i + "] " + (i + 1) + "x" + (i + 1));
             mat3[i] = new Matrix(i + 1, i + 1);
-            mat3[i].setRandomInt(0,  10 * (int)Math.pow(4, i));
+            mat3[i].setRandomInt(0, 10 * (int) Math.pow(4, i));
             mat3[i].print();
             System.out.println();
         }
+
+        // Тест операций с матрицами
+        Matrix[] mat4 = new Matrix[4];
+        mat4[0] = new Matrix(3, 3);
+        mat4[1] = new Matrix(3, 3);
+        mat4[2] = new Matrix(5, 2);
+        mat4[3] = new Matrix(2, 4);
+
+        mat4[0].setDiagonal(127);
+        mat4[1].setRandomInt(-5, 5);
+        mat4[2].setRandomInt(-5, 5);
+        mat4[3].setRandomInt(-5, 5);
+
+        // sum & difference
+        System.out.println();
+        mat4[0].print();
+        System.out.println(" +");
+        mat4[1].print();
+        System.out.println(" =");
+        Matrix m1 = Matrix.sum(mat4[0], mat4[1]);
+        m1.print();
+        System.out.println(" -");
+        mat4[0].print();
+        System.out.println(" =");
+        Matrix m2 = Matrix.difference(m1, mat4[0]);
+        m2.print();
+
+        // multiply(Matrix, Matrix)
+        System.out.println();
+        System.out.println("multiply(Matrix, Matrix):");
+        mat4[2].print();
+        System.out.println(" *");
+        mat4[3].print();
+        System.out.println(" =");
+        Matrix m3 = Matrix.multiply(mat4[2], mat4[3]);
+        m3.print();
+
+        // multiply(Matrix, Vector)
+        System.out.println();
+        System.out.println("multiply(Matrix, Vector):");
+        Vector v1 = new Vector(4);
+        v1.setRandomInt(0, 5);
+        mat4[3].print();
+        System.out.println(" *");
+        System.out.println(v1);
+        System.out.println(" =");
+        Vector v2 = Matrix.multiply(mat4[3],v1);
+        System.out.println(v2);
+        System.out.println();
+
+        // multiply(Vector, Vector)
+        System.out.println();
+        System.out.println("multiply(Vector, Vector):");
+        System.out.println(v1);
+        System.out.println(" *");
+        System.out.println(v2);
+        System.out.println(" =");
+        Matrix m4 = Matrix.multiply(v2, v1);
+        m4.print();
+
+        // multiply(Matrix, double)
+        System.out.println();
+        System.out.println("multiply(Matrix, double):");
+        mat4[0].print();
+        System.out.println(" * 2");
+        mat4[0].multiply(2);
+        mat4[0].print();
+
+        // transpose()
+        System.out.println();
+        System.out.println();
+        mat4[3].print();
+        System.out.println(" transpose:");
+        mat4[3].transpose();
+        mat4[3].print();
     }
 }
