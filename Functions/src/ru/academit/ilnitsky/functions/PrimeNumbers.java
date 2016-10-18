98f1883d1129a2eb4572f996b2d5cd3d2b652379
m-ilnitsky <ilnitsky@ngs.ru>
2016-10-18 20:09:17 +0700

    Prime Numbers

--- Functions/src/ru/academit/ilnitsky/functions/PrimeNumbers.java
+++ Functions/src/ru/academit/ilnitsky/functions/PrimeNumbers.java
@@ -13,9 +13,18 @@ public class PrimeNumbers {
 
     // Создаём массив простых чисел в диапазоне от 2 до maxValue
     public PrimeNumbers(int maxValue) {
-        int arraySize;
+        int arraySize = 1 + (int) (maxValue / Math.log10(maxValue) / Math.sqrt(Math.log10(maxValue) / 2));
         // Определяем размер массива простых чисел
-        if (maxValue > 4000) {
+        /*
+        if (maxValue > 2e7) {
+            arraySize = maxValue / 14;
+        } else if (maxValue > 2e6) {
+            arraySize = maxValue / 12;
+        } else if (maxValue > 2e5) {
+            arraySize = maxValue / 10;
+        } else if (maxValue > 2e4) {
+            arraySize = maxValue / 8;
+        } else if (maxValue > 4000) {
             arraySize = maxValue / 7;
         } else if (maxValue > 1500) {
             arraySize = maxValue / 6;
@@ -30,6 +39,7 @@ public class PrimeNumbers {
         } else {
             arraySize = maxValue / 2 + 1;
         }
+        */
 
         int[] primeNumber = new int[arraySize];
         primeNumber[0] = 2;
@@ -79,6 +89,66 @@ public class PrimeNumbers {
         numbers = resultArray;
     }
 
+    // Расширяем массив простых чисел до maxValue
+    public void expandTo(int maxValue) {
+        if (maxValue <= numbers[numbers.length - 1]) {
+            System.out.println("Из имеющихся [" + numbers.length + "] чисел максимальное число (" + numbers[numbers.length - 1] + ") уже больше заданного (" + maxValue + ")");
+            return;
+        }
+
+        int oldSize = numbers.length;
+        int arraySize = 1 + (int) (maxValue / Math.log10(maxValue) / Math.sqrt(Math.log10(maxValue) / 2));
+
+        System.out.println("Old Size=" + oldSize);
+        System.out.println("New Size=" + arraySize);
+
+        int[] primeNumber = new int[arraySize];
+        System.arraycopy(numbers, 0, primeNumber, 0, oldSize);
+
+        numbers = primeNumber;
+
+        int numberOfPrime = oldSize;
+
+        for (int i = numbers[oldSize - 1] + 1; i <= maxValue; i++) {
+            if (i % 2 == 0) {
+                continue;
+            }
+
+            int half = i / 2;
+            boolean isNotPrime = false;
+
+            if (numberOfPrime > 0) { // Проверяем делением на числа из заданного диапазона
+                for (int j = 1; primeNumber[j] < half; j++) {
+                    if (i % primeNumber[j] == 0) {
+                        //System.out.printf("B.NonPrime: %d / %d [%d]%n", i, primeNumber[j], j + 1);
+                        isNotPrime = true;
+                        break;
+                    }
+                }
+            }
+
+            if (isNotPrime) {
+                continue;
+            }
+
+            primeNumber[numberOfPrime] = i;
+            numberOfPrime++;
+        }
+
+        // Урезаем массив до количества простых чисел в нём
+        int[] resultArray;
+        if (numberOfPrime == primeNumber.length) {
+            resultArray = primeNumber;
+        } else {
+            resultArray = new int[numberOfPrime];
+
+            System.arraycopy(primeNumber, 0, resultArray, 0, numberOfPrime);
+        }
+        numbers = resultArray;
+
+        System.out.println("Result Size=" + numbers.length);
+    }
+
     // Бинарный поиск в отсортированном массиве
     public int findIndex(int number) {
         int left = 0;
@@ -103,7 +173,63 @@ public class PrimeNumbers {
         return -1;
     }
 
-    public void saveToFile(String fileName, String fileCode) throws IOException {
+    public int findEqualOrLarger(int number) {
+        int left = 0;
+        int right = numbers.length - 1;
+
+        if (number < numbers[left] || number > numbers[right]) {
+            return -1;
+        }
+
+        while (left <= right) {
+            int middle = (left + right) / 2;
+
+            if (number == numbers[middle]) {
+                return middle;
+            } else if (number > numbers[middle]) {
+                left = middle + 1;
+            } else {
+                right = middle - 1;
+            }
+        }
+
+        if (numbers[left] > number) {
+            return left;
+        } else {
+            return left + 1;
+        }
+
+    }
+
+    public int findEqualOrSmaller(int number) {
+        int left = 0;
+        int right = numbers.length - 1;
+
+        if (number < numbers[left] || number > numbers[right]) {
+            return -1;
+        }
+
+        while (left <= right) {
+            int middle = (left + right) / 2;
+
+            if (number == numbers[middle]) {
+                return middle;
+            } else if (number > numbers[middle]) {
+                left = middle + 1;
+            } else {
+                right = middle - 1;
+            }
+        }
+
+        if (numbers[right] < number) {
+            return right;
+        } else {
+            return right - 1;
+        }
+
+    }
+
+    public void saveToFileFormat(String fileName, String fileCode) throws IOException {
 
         try (
                 PrintWriter writer = new PrintWriter(fileName, fileCode)
@@ -114,9 +240,20 @@ public class PrimeNumbers {
         }
     }
 
-    public void saveToFile(String fileName) throws IOException {
+    public void saveToFileFormat(String fileName) throws IOException {
         String fileCode = "ASCII";
-        saveToFile(fileName, fileCode);
+        saveToFileFormat(fileName, fileCode);
+    }
+
+    public void saveToFile(String fileName) throws IOException {
+
+        try (
+                PrintWriter writer = new PrintWriter(fileName, "ASCII")
+        ) {
+            for (int i = 0; i < numbers.length; i++) {
+                writer.println(numbers[i] + " " + (i + 1));
+            }
+        }
     }
 
     public void print(int numFirstPrimeNumbers) {
@@ -128,18 +265,21 @@ public class PrimeNumbers {
 
     public void print(int begin, int end) {
         int count = 0;
-        for (int i = 0; i < numbers.length; i++) {
-            int num = numbers[i];
-            if (num >= begin && num <= end) {
-                count++;
-                System.out.printf("%d [%d] = %d%n", count, i + 1, numbers[i]);
-            }
+        int iBegin = findEqualOrLarger(begin);
+        int iEnd = findEqualOrSmaller(end);
+
+        for (int i = iBegin; i <= iEnd; i++) {
+            count++;
+            System.out.printf("%d [%d] = %d%n", count, i + 1, numbers[i]);
         }
     }
 
     public static void main(String[] args) throws IOException {
-        PrimeNumbers prime = new PrimeNumbers(1000000000);
-        prime.print(999999900, 1000000000);
-        prime.saveToFile("prime_numbers_1e9.txt");
+        PrimeNumbers prime = new PrimeNumbers(100);
+        prime.expandTo(50);
+        prime.expandTo(100);
+        prime.expandTo(1000);
+        prime.expandTo(100);
+        prime.saveToFile("prime_numbers_1e3_expand_from_100.txt");
     }
 }
