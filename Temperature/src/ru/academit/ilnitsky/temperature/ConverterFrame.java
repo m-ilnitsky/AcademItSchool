3710ca32961cf0be974de73c3fe46248920867f0
m-ilnitsky <ilnitsky@ngs.ru>
2016-12-09 19:45:18 +0700

    Temperature: небольшое продвижение.

--- Temperature/src/ru/academit/ilnitsky/temperature/ConverterFrame.java
+++ Temperature/src/ru/academit/ilnitsky/temperature/ConverterFrame.java
@@ -9,14 +9,14 @@ import java.awt.*;
  */
 public class ConverterFrame extends JFrame {
     ConverterFrame() {
-        super("Преобразование температур");
+        super("Конвертер");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(250, 250);
 
         JPanel inputPanel = new JPanel();
         JPanel outputPanel = new JPanel();
         add(inputPanel, BorderLayout.NORTH);
-        add(outputPanel, BorderLayout.SOUTH);
+        add(outputPanel, BorderLayout.CENTER);
 
         JComboBox type = new JComboBox();
         JTextField text = new JTextField();
@@ -27,26 +27,48 @@ public class ConverterFrame extends JFrame {
         button.setSize(80, 20);
         button.setText("Ok");
 
-        inputPanel.setLayout(new FlowLayout());
+        inputPanel.setLayout(new GridLayout(1,3));
         inputPanel.add(type);
         inputPanel.add(text);
         inputPanel.add(button);
 
-        JLabel labelK = new JLabel("Температура в К : ");
-        JLabel labelC = new JLabel("Температура в C : ");
-        JLabel labelF = new JLabel("Температура в F : ");
+        JLabel labelGroup1      = new JLabel("Заданные температуры:");
+        JLabel labelK           = new JLabel("Температура в К : ");
+        JLabel labelC           = new JLabel("Температура в C : ");
+        JLabel labelF           = new JLabel("Температура в F : ");
+        JLabel labelGroup2      = new JLabel("Энергия для данной температуры:");
+        JLabel labelJ           = new JLabel("Энергия     в Дж: ");
+        JLabel labelErg         = new JLabel("Энергия    в Эрг: ");
+        JLabel labelEV          = new JLabel("Энергия     в эВ: ");
+        JLabel labelGroup3      = new JLabel("Длина волны макисмума интенсивности излучения для данной температуры:");
+        JLabel labelLambdaTm    = new JLabel("Длина волны макисмума интенсивности,   м ");
+        JLabel labelLambdaTum   = new JLabel("Длина волны макисмума интенсивности, мкм ");
+        JLabel labelGroup4      = new JLabel("Параметры излучения в вакууме для данной энергии:");
+        JLabel labelFrequency   = new JLabel("Частота излучения,   Гц ");
+        JLabel labelLambdaEm    = new JLabel("Длина волны излучения,   м ");
+        JLabel labelLambdaEum   = new JLabel("Длина волны излучения, мкм ");
 
-        JLabel resultK = new JLabel("0");
-        JLabel resultC = new JLabel("0");
-        JLabel resultF = new JLabel("0");
+        JLabel resultGroup1 = new JLabel("");
+        JLabel resultGroup2 = new JLabel("");
+        JLabel resultGroup3 = new JLabel("");
+        JLabel resultGroup4 = new JLabel("");
 
-        outputPanel.setLayout(new GridLayout(3,2));
+        JLabel resultK = new JLabel("01234567890");
+        JLabel resultC = new JLabel("01234567890");
+        JLabel resultF = new JLabel("01234567890");
+
+        outputPanel.setLayout(new GridLayout(8,2));
+        outputPanel.add(labelGroup1);
+        outputPanel.add(resultGroup1);
         outputPanel.add(labelK);
         outputPanel.add(resultK);
         outputPanel.add(labelC);
         outputPanel.add(resultC);
         outputPanel.add(labelF);
         outputPanel.add(resultF);
+
+        outputPanel.add(labelGroup2);
+        outputPanel.add(resultGroup2);
     }
 
     public static void main(String... arg) {
--- Temperature/src/ru/academit/ilnitsky/temperature/EnergyConverter.java
+++ Temperature/src/ru/academit/ilnitsky/temperature/EnergyConverter.java
@@ -5,27 +5,100 @@ package ru.academit.ilnitsky.temperature;
  * Created by UserLabView on 08.12.16.
  */
 public class EnergyConverter extends TemperatureConverter {
-    public static final double k = 1.38064852e-23;
+    public static final double e = 1.6021766208e-19;    // [C]
+    public static final double k = 1.38064852e-23;      // [J/K]
+    public static final double h = 6.62607004e-34;      // [J*s]
+    public static final double c = 299792458;           // [m/s]
+    public static final double lambdaTm = 0.0028999;    // [m*K]
+    public static final double lambdaTum = 2899.9;      // [um*K]
 
-    private double energyJ = 0;
+    protected double energyJ = 0;
+
+    protected void setJfromK() {
+        energyJ = temperatureK * k;
+    }
+
+    protected void setKfromJ() {
+        temperatureK = energyJ / k;
+    }
 
     public void setK(double tK) {
         super.setK(tK);
-        energyJ = temperatureK * k;
+        setJfromK();
     }
 
     public void setC(double tC) {
         super.setC(tC);
-        energyJ = temperatureK * k;
+        setJfromK();
     }
 
     public void setF(double tF) {
         super.setF(tF);
-        energyJ = temperatureK * k;
+        setJfromK();
     }
 
-    public void setJ(double eJ) {
+    public void setEnergyJ(double eJ) {
         energyJ = eJ;
-        temperatureK = eJ / k;
+        setKfromJ();
+    }
+
+    public void setEnergyEV(double eEV) {
+        setEnergyJ(eEV * e);
+    }
+
+    public void setEnergyErg(double eErg) {
+        setEnergyJ(eErg * 1e-7);
+    }
+
+    public void setLambdaTm(double lambda) {
+        setK(lambdaTm / lambda);
+    }
+
+    public void setLambdaTum(double lambda) {
+        setK(lambdaTum / lambda);
+    }
+
+    public void setFrequency(double hz) {
+        setEnergyJ(hz * h);
+    }
+
+    public void setLambdaEm(double lambda) {
+        setEnergyJ(h * c / lambda);
+    }
+
+    public void setLambdaEum(double lambda) {
+        setLambdaEm(lambda / 1e6);
+    }
+
+    public double getEnergyJ() {
+        return energyJ;
+    }
+
+    public double getEnergyEV() {
+        return energyJ / e;
+    }
+
+    public double getEnergyErg() {
+        return energyJ / 1e-7;
+    }
+
+    public double getLambdaTm() {
+        return lambdaTm / temperatureK;
+    }
+
+    public double getLambdaTum() {
+        return lambdaTum / temperatureK;
+    }
+
+    public double getFrequency() {
+        return energyJ / h;
+    }
+
+    public double getLambdaEm() {
+        return h * c / energyJ;
+    }
+
+    public double getLambdaEum() {
+        return getLambdaEm() * 1e6;
     }
 }
