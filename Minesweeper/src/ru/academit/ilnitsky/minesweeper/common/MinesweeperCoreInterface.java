c2da82e4fd77868888f7085bdfb2720462147f2a
Mike <ilnitsky@ngs.ru>
2017-02-02 04:06:11 +0700

    Minesweeper: начало.

--- Minesweeper/src/ru/academit/ilnitsky/minesweeper/common/MinesweeperCoreInterface.java
+++ Minesweeper/src/ru/academit/ilnitsky/minesweeper/common/MinesweeperCoreInterface.java
@@ -4,7 +4,7 @@ package ru.academit.ilnitsky.minesweeper.common;
  * Интерфейс модели игры "Минёр"
  * Created by Mike on 02.02.2017.
  */
-public interface MinesweeperGameCore {
+public interface MinesweeperCoreInterface {
 
     void setGameBoardSize(int xSize, int ySize);
 
