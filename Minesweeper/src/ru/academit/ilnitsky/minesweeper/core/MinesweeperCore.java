e0b1b7639e77a5146cfca2001d558b95c8f891ad
Mike <ilnitsky@ngs.ru>
2017-02-03 03:02:43 +0700

    Minesweeper: работа над ядром 2.

--- Minesweeper/src/ru/academit/ilnitsky/minesweeper/core/MinesweeperCore.java
+++ Minesweeper/src/ru/academit/ilnitsky/minesweeper/core/MinesweeperCore.java
@@ -1,11 +1,12 @@
 package ru.academit.ilnitsky.minesweeper.core;
 
-import ru.academit.ilnitsky.minesweeper.common.CellState;
-import ru.academit.ilnitsky.minesweeper.common.GameBoard;
-import ru.academit.ilnitsky.minesweeper.common.GameBoardSize;
-import ru.academit.ilnitsky.minesweeper.common.MinesweeperCoreInterface;
+import ru.academit.ilnitsky.minesweeper.common.*;
+
+import java.time.LocalTime;
+import java.util.Random;
 
 /**
+ * Ядро (модель) игры "Сапёр"
  * Created by UserLabView on 02.02.17.
  */
 public class MinesweeperCore implements MinesweeperCoreInterface {
@@ -16,21 +17,34 @@ public class MinesweeperCore implements MinesweeperCoreInterface {
     private int[][] visibleBoard;
     private GameBoard outputBoard;
 
+    private LocalTime time = LocalTime.now();
+
     private boolean isStarted;
     private boolean isEnded;
+    private boolean isDetonated;
+
     private int numFlagActions;
     private int numActions;
     private int startTime;
+    private int currentTime;
+    private int stopTime;
 
-    private final static double mineCoefficient = 2.0 / 3.0;
+    private final static double MINE_COEFFICIENT = 0.3;
+    private final static int MIN_SIZE = 5;
+    private final static int MAX_SIZE = 64;
 
     @Override
     public void setGameBoardSize(int xSize, int ySize) {
-        if (xSize < 5) {
-            throw new IllegalArgumentException("xSize < 5");
+        if (xSize < MIN_SIZE) {
+            throw new IllegalArgumentException("xSize < MIN_SIZE");
+        } else if (xSize > MAX_SIZE) {
+            throw new IllegalArgumentException("xSize > MAX_SIZE");
         }
-        if (ySize < 5) {
-            throw new IllegalArgumentException("ySize < 5");
+
+        if (ySize < MIN_SIZE) {
+            throw new IllegalArgumentException("ySize < MIN_SIZE");
+        } else if (ySize > MAX_SIZE) {
+            throw new IllegalArgumentException("ySize > MAX_SIZE");
         }
 
         this.xSize = xSize;
@@ -40,7 +54,7 @@ public class MinesweeperCore implements MinesweeperCoreInterface {
 
     @Override
     public void setGameNumMine(int numMines) {
-        int maxNumMines = (int) (mineCoefficient * xSize * ySize);
+        int maxNumMines = (int) (MINE_COEFFICIENT * xSize * ySize);
 
         if (numMines > maxNumMines) {
             throw new IllegalArgumentException("numMines > max (" + maxNumMines + ")");
@@ -54,15 +68,15 @@ public class MinesweeperCore implements MinesweeperCoreInterface {
     public void initNewGame() {
         isStarted = false;
         isEnded = false;
+        isDetonated = false;
+
         numActions = 0;
         numFlagActions = 0;
     }
 
     public void startNewGame() {
+        initNewGame();
         isStarted = true;
-        isEnded = false;
-        numActions = 0;
-        numFlagActions = 0;
 
         hiddenBoard = new int[xSize][];
         visibleBoard = new int[xSize][];
@@ -104,18 +118,23 @@ public class MinesweeperCore implements MinesweeperCoreInterface {
         return 0;
     }
 
-    @Override
-    public void setFlag(int xPosition, int yPosition) {
+    private void checkPosition(int xPosition, int yPosition) {
         if (xPosition < 0) {
             throw new IllegalArgumentException("xPosition < 0");
         } else if (xPosition >= xSize) {
             throw new IllegalArgumentException("xPosition >= xSize");
         }
+
         if (yPosition < 0) {
             throw new IllegalArgumentException("yPosition < 0");
         } else if (yPosition >= ySize) {
             throw new IllegalArgumentException("yPosition >= ySize");
         }
+    }
+
+    @Override
+    public void setFlag(int xPosition, int yPosition) {
+        checkPosition(xPosition, yPosition);
 
         if (visibleBoard[xPosition][yPosition] == CellState.CLOSE.getValue()) {
             visibleBoard[xPosition][yPosition] = CellState.FLAG.getValue();
@@ -130,21 +149,170 @@ public class MinesweeperCore implements MinesweeperCoreInterface {
 
     @Override
     public boolean setOpen(int xPosition, int yPosition) {
+        checkPosition(xPosition, yPosition);
+
         if (isEnded || !isStarted) return false;
 
         if (numActions == 0) {
-            firstStep(xPosition, yPosition);
-            return true;
-        } else {
-            return nextStep(xPosition, yPosition);
+            initCellsForFirstPosition(xPosition, yPosition);
+        }
+
+        return makeStep(xPosition, yPosition);
+    }
+
+    private Position[] getPositionOfMines() {
+        Random random = new Random(time.toNanoOfDay());
+
+        Position[] positions = new Position[numMines];
+
+        for (int i = 0; i < numMines; i++) {
+            positions[i].setX(random.nextInt(xSize));
+            positions[i].setY(random.nextInt(ySize));
+        }
+
+        return positions;
+    }
+
+    private int calcNumMinesAround(int xPosition, int yPosition) {
+        if (hiddenBoard[xPosition][yPosition] == CellState.MINE.getValue()) {
+            return hiddenBoard[xPosition][yPosition];
+        }
+
+        int xStart = xPosition - 1;
+        int xEnd = xPosition + 1;
+        int yStart = yPosition - 1;
+        int yEnd = yPosition + 1;
+
+        if (xPosition == 0) {
+            xStart = 0;
+        }
+        if (yPosition == 0) {
+            yStart = 0;
+        }
+        if (xPosition == (xSize - 1)) {
+            xEnd = xSize - 1;
+        }
+        if (yPosition == (ySize - 1)) {
+            yEnd = ySize - 1;
+        }
+
+        int numMines = 0;
+        int mineValue = CellState.MINE.getValue();
+
+        for (int iX = xStart; iX <= xEnd; iX++) {
+            for (int iY = yStart; iY <= yEnd; iY++) {
+                if (hiddenBoard[iX][iY] == mineValue) {
+                    numMines++;
+                }
+            }
+        }
+
+        return numMines;
+    }
+
+    private void initCellsForFirstPosition(int xPosition, int yPosition) {
+        boolean isNonAdequatePositions;
+        Position[] minePositions;
+
+        do {
+            minePositions = getPositionOfMines();
+            isNonAdequatePositions = false;
+
+            for (Position p : minePositions) {
+                if (p.getX() == xPosition && p.getY() == yPosition) {
+                    isNonAdequatePositions = true;
+                }
+            }
+        } while (isNonAdequatePositions);
+
+        for (Position p : minePositions) {
+            hiddenBoard[p.getX()][p.getY()] = CellState.MINE.getValue();
+        }
+
+        for (int i = 0; i < xSize; i++) {
+            for (int j = 0; j < ySize; j++) {
+                hiddenBoard[i][j] = calcNumMinesAround(i, j);
+            }
         }
     }
 
-    private void firstStep(int xPosition, int yPosition) {
-
+    private boolean isMine(int xPosition, int yPosition) {
+        return hiddenBoard[xPosition][yPosition] == CellState.MINE.getValue();
     }
 
-    private boolean nextStep(int xPosition, int yPosition) {
-        return false;
+    private boolean isFree(int xPosition, int yPosition) {
+        return hiddenBoard[xPosition][yPosition] == CellState.FREE.getValue();
+    }
+
+    private boolean isNumber(int xPosition, int yPosition) {
+        return hiddenBoard[xPosition][yPosition] >= CellState.N1.getValue()
+                && hiddenBoard[xPosition][yPosition] <= CellState.N8.getValue();
+    }
+
+    private void showHiddenBoard() {
+        for (int i = 0; i < xSize; i++) {
+            System.arraycopy(hiddenBoard[i], 0, visibleBoard[i], 0, ySize);
+        }
+    }
+
+    private boolean makeStep(int xPosition, int yPosition) {
+        numActions++;
+
+        if (isMine(xPosition, yPosition)) {
+            showHiddenBoard();
+            visibleBoard[xPosition][yPosition] = CellState.DETONATION.getValue();
+            isDetonated = true;
+            return false;
+
+        } else if (isNumber(xPosition, yPosition)) {
+            visibleBoard[xPosition][yPosition] = hiddenBoard[xPosition][yPosition];
+
+        } else if (isFree(xPosition, yPosition)) {
+            showFreeCellsAround(xPosition, yPosition);
+
+        } else {
+            throw new IllegalArgumentException("Unknown Cell State: hiddenBoard[ "
+                    + xPosition + " ][ " + yPosition + "] = " + hiddenBoard[xPosition][yPosition]);
+        }
+
+        if (outputBoard.getNumCells(CellState.CLOSE) + outputBoard.getNumCells(CellState.FLAG) == numMines) {
+            isEnded = true;
+        }
+
+        return true;
+    }
+
+    private void showFreeCellsAround(int xPosition, int yPosition) {
+        int xStart = xPosition - 1;
+        int xEnd = xPosition + 1;
+        int yStart = yPosition - 1;
+        int yEnd = yPosition + 1;
+
+        if (xPosition == 0) {
+            xStart = 0;
+        }
+        if (yPosition == 0) {
+            yStart = 0;
+        }
+        if (xPosition == (xSize - 1)) {
+            xEnd = xSize - 1;
+        }
+        if (yPosition == (ySize - 1)) {
+            yEnd = ySize - 1;
+        }
+
+        for (int iX = xStart; iX <= xEnd; iX++) {
+            for (int iY = yStart; iY <= yEnd; iY++) {
+                if (iX != xPosition && iY != yPosition) {
+                    if (isNumber(iX, iY)) {
+                        visibleBoard[iX][iY] = hiddenBoard[iX][iY];
+                    } else if (isFree(iX, iY)) {
+                        showFreeCellsAround(iX, iY);
+                    }
+                } else {
+                    visibleBoard[iX][iY] = CellState.FREE.getValue();
+                }
+            }
+        }
     }
 }
